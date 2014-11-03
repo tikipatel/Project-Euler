b085b8bfeefc750239902ef099d116cc421f77eb
Pratikbhai Patel <pratikpatel@me.com>
2014-11-02 21:50:32 -0500

    Problem #10 in Python

---
+++ Problem10.py
@@ -0,0 +1,19 @@
+import numpy
+
+def sievePrimes(maximum):
+    primes = dict.fromkeys(range(3,maximum+1,2),True)
+    sieveList = [2]
+    for num in sorted(primes):
+        if primes[num] == True:
+            sieveList.append(num)
+            j = num ** 2
+            while j < maximum:
+                if j in primes:
+                    primes[j] = False
+                j += num
+    return(sieveList)
+
+x = sievePrimes(2000000)
+print(sum(x))
+
+
