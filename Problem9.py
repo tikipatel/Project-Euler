e76f00620825c1123b918738dc66cbe48bda3b97
Pratikbhai Patel <pratikpatel@me.com>
2014-11-03 17:54:38 -0500

    Problem 9 update

--- Problem9.py
+++ Problem9.py
@@ -1,7 +1,13 @@
 from math import sqrt
 
-for a in range(1,1000):
-	for b in range(a+1,1000):
+#for a in range(1,1000):
+#	for b in range(a+1,1000):
+#		c = (a**2 + b**2)**0.5
+#		if  a + b + c == 1000:
+#			print(a, b, c, a*b*c)
+			
+for a in range(1,500):
+	for b in range(a+1, 500):
 		c = (a**2 + b**2)**0.5
-		if  a + b + c == 1000:
-			print(a, b, c, a*b*c)
+		if a + b + c == 1000:
+			print(a,b,c, a*b*c)
